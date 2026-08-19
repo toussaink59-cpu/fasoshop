07fd4c9e0e9194340b6554a9bb65ec94c27b46c5
KIEMDE P. Toussaint <toussaink59@gmail.com>
2026-08-19 11:23:41 +0000

    feat(pwa): add PNG icons 192/512 for Chrome installability criteria

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Kimoxa" />
-        <link rel="apple-touch-icon" href="/icons/icon.svg" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         {process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN && (
           <Script
             strategy="afterInteractive"
